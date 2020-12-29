1f6110a7250b39886d104f37b426c87bf01ce69d
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-29 17:08:05 +0530

    Added code for daily EmpWage

--- src/com/javaprogram/employeewage/EmployeeWage.java
+++ src/com/javaprogram/employeewage/EmployeeWage.java
@@ -8,18 +8,22 @@ public class EmployeeWage {
 	private static final int absent=0;
 	
 	public static void main(String[] args) {
+		final int wagePerHour=20;
+		
 		System.out.println("Welcome to employee wage computation program");
 		Random random=new Random();
-		int attendace=random.nextInt(2);
-		if(attendace == present)
+		int attendance=random.nextInt(2);
+		if(attendance == present)
 		{
-			System.out.println("Present "+attendace);
+			System.out.println("Present "+attendance);
 		}
 		else
 		{
-			System.out.println("absent "+attendace);
+			System.out.println("absent "+attendance);
 		}
-			
+		int dailyEmpWage=attendance*wagePerHour;
+		System.out.println("Daily Emp Wage:"+dailyEmpWage);
+		
 	}
 
 }
